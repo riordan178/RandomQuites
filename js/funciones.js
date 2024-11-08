2c52a845301191d9e63e8145e15da9bb0fa6f402
Edgar Suncion <edgarjon@hotmail.com>
2024-11-07 21:25:37 -0500

    Avance 07/11/24

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,28 @@
-export function Saludo(){
-    return "Hola mundo"
+export function test(){
+    alert('funciona el click nuevo quote')
+}
+
+export async function CallAPI(){
+    const data = await fetch('https://api.api-ninjas.com/v1/quotes',{
+       headers:{
+        'X-Api-Key':'wsjwb/2q27gLuMO8SfFqgg==TWP31tLqh54vKGjB'
+       }
+    });
+    const datos= await data.json();
+    return datos    
+}
+
+export async function LoadQuote(quotehtml,autor,tag) {
+    const dataAPI= await CallAPI();
+    const {quote,author,category}=dataAPI[0];
+    autor[0].innerText=author;
+    tag[0].innerHTML=`<p>${category}</p>`;
+    quotehtml[0].innerText=quote;
+}
+
+export  function CopyQuote(quotehtml) {
+    navigator.clipboard.writeText(quotehtml[0].textContent)
+    .then(() => {
+      console.log('Texto copiado al portapapeles');
+    })
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-import { Sumar } from "./funciones";
-const titulo= document.querySelector('div');
-titulo.innerHTML=Sumar()
+import {test,LoadQuote,CopyQuote} from "./funciones.js"
+
+const QuoteHTML=document.getElementsByClassName('quote');
+const Author=document.getElementsByClassName('author');
+const Tags= document.getElementsByClassName('tags');
+//Cargando frase al cargar la pagina
+document.addEventListener('DOMContentLoaded',()=>{
+    LoadQuote(QuoteHTML,Author,Tags)
+    
+})
+
+//Cargando frase al dar click a nueva frase
+document.addEventListener("click",(e)=>{
+    (e.target.id=="newquote") && LoadQuote(QuoteHTML,Author,Tags);
+    (e.target.id=="link") && CopyQuote(QuoteHTML);
+})
+
+
+//Copiando la frase al dar click a copiar
+
+
